0c6470ccff265c2972f8dd2b4a7cc7a4fa0f7390
Alejandro Castanon <alex.castanon4572@gmail.com>
2018-08-27 20:31:38 -0600

    FINAL

---
+++ basededatos/basededatos/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace basededatos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(49, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Nuevo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(49, 73);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(140, 26);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Consultas";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(49, 161);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(140, 25);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Borrar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(384, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(153, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(384, 73);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(153, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(384, 118);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(153, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(384, 161);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(153, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(49, 118);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(140, 26);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Actualizar";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(543, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "No. de control";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(543, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(543, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Apellido Paterno";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(541, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Apellido Materno";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(49, 210);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(578, 235);
+            this.dataGridView1.TabIndex = 13;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 497);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ basededatos/basededatos/Form1.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace basededatos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                string connectionString = "Server=localhost\\SQLEXPRESS;Database=Escuela;Trusted_connection=True";
+
+                SqlConnection conexion;
+
+                conexion = new SqlConnection(connectionString);
+
+                SqlCommand comando;
+
+                string query = "INSERT INTO Alumnos " +
+                    "(no_control, nombre, apaterno, amaterno)" + "VALUES ('" + textBox1.Text + "', '" + textBox2.Text + "', '" + textBox3.Text + "', '" + textBox4.Text + "')";
+
+
+                try
+                {
+                    conexion.Open();
+
+                    comando = new SqlCommand(query, conexion);
+
+                    comando.ExecuteNonQuery();
+
+                    MessageBox.Show("Registro Guardado");
+
+                    comando.Dispose();
+
+                    conexion.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+                Load_Datagrid();
+                Limpiar();
+            }
+        }
+
+            private void button2_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Server=localhost\\SQLEXPRESS;Database=Escuela;Trusted_connection=True";
+
+            SqlConnection conexion;
+
+            conexion = new SqlConnection(connectionString);
+
+            SqlCommand comando;
+
+            string query = "INSERT INTO Alumnos VALUES (152050, 'Alejandro' , 'Castanon', 'Cardona')";
+
+            try
+            {
+                conexion.Open();
+
+                comando = new SqlCommand(query, conexion);
+
+                comando.ExecuteNonQuery();
+
+                MessageBox.Show("Registro Guardado");
+
+                comando.Dispose();
+
+                conexion.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            Load_Datagrid();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string conexionString = "Server=localhost\\SQLEXPRESS;Database=Escuela;Trusted_connection=True";
+
+            SqlConnection conexion;
+
+            conexion = new SqlConnection(conexionString);
+
+            SqlCommand comando;
+
+
+
+            string query = "select * from Alumnos where nombre='" + textBox2.Text + "'";
+
+            try
+            {
+                conexion.Open();
+
+                comando = new SqlCommand(query, conexion);
+                SqlDataAdapter da = new SqlDataAdapter(comando);
+                DataTable dataTable = new DataTable();
+
+                da.Fill(dataTable);
+
+                dataGridView1.DataSource = dataTable;
+
+                dataTable.Columns[0].ColumnName = "no_control";
+                dataTable.Columns[1].ColumnName = "Nombre";
+                dataTable.Columns[2].ColumnName = "A_Paterno";
+                dataTable.Columns[3].ColumnName = "A_Materno";
+
+
+                comando.Dispose();
+                conexion.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+            Limpiar();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Server=localhost\\SQLEXPRESS;Database=Escuela;Trusted_connection=True";
+
+            SqlConnection conexion;
+
+            conexion = new SqlConnection(connectionString);
+
+            SqlCommand comando;
+
+            string query = "Delete from Alumnos where nombre='" + textBox2.Text + "'";
+            try
+            {
+                conexion.Open();
+
+                comando = new SqlCommand(query, conexion);
+
+                comando.ExecuteNonQuery();
+
+                MessageBox.Show("Registro Eliminado");
+
+                comando.Dispose();
+
+                conexion.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            Load_Datagrid();
+            Limpiar();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Load_Datagrid();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBox1.Text = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+            textBox2.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            textBox3.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+            textBox4.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+        }
+        public void Limpiar()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+        }
+    
+        public void Load_Datagrid()
+        {
+            string conexionString = "Server=localhost\\SQLEXPRESS;Database=Escuela;Trusted_connection=True";
+
+            SqlConnection conexion;
+
+            conexion = new SqlConnection(conexionString);
+
+            SqlCommand comando;
+
+            string query;
+
+            query = "SELECT * FROM Alumnos";
+
+
+
+            try
+            {
+                conexion.Open();
+
+                comando = new SqlCommand(query, conexion);
+
+                SqlDataAdapter da = new SqlDataAdapter(comando);
+
+                DataTable datatable = new DataTable();
+
+                da.Fill(datatable);
+
+                dataGridView1.DataSource = datatable;
+
+                datatable.Columns[0].ColumnName = "No_Control";
+                datatable.Columns[1].ColumnName = "Nombre";
+                datatable.Columns[2].ColumnName = "A_Paterno";
+                datatable.Columns[3].ColumnName = "A_Materno";
+
+                comando.Dispose();
+
+                conexion.Close();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            {
+                Load_Datagrid();
+            }
+
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string conexionString = "Server=localhost\\SQLEXPRESS;Database=Escuela;Trusted_connection=True";
+
+            SqlConnection conexion;
+
+            conexion = new SqlConnection(conexionString);
+
+            SqlCommand comando;
+
+            SqlDataReader dataReader;
+
+            string query = "UPDATE Alumnos set nombre='" + textBox2.Text + "',apaterno='" + textBox3.Text + "',amaterno='" + textBox4.Text + "' where no_control=" + textBox1.Text;
+            MessageBox.Show("Registro actualizado");
+            string datos = "...";
+
+            try
+            {
+                conexion.Open();
+
+                comando = new SqlCommand(query, conexion);
+
+                dataReader = comando.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    //MessageBox.Show("no_Control " + dataReader.GetValue(0) + " Nombre " + dataReader.GetValue(1));
+
+                    datos += ("no_Control " + dataReader.GetValue(0) + " nombre " + dataReader.GetValue(1) + "\n");
+                }
+
+                MessageBox.Show(datos);
+
+                comando.Dispose();
+
+                conexion.Close();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            Load_Datagrid();
+            Limpiar();
+        }
+    }
+    }
+    
+
